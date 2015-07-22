4ed0266ff31fd882c54b6edbe8e18dd1ebf857ae
Chip Reed <chip@chip.sh>
2015-07-22 03:49:13 -0700

    make event images have proper thumbnails for faster loading

---
+++ js/noscript.js
@@ -0,0 +1,6 @@
+(function(){
+    var elements = document.querySelectorAll(".noscript");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'inherit';
+    }
+})();
