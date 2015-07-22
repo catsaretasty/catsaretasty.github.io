dd130d6b7aac2298b4b06600a149df06ac098389
Chip Reed <chip@chip.sh>
2015-07-22 00:56:16 -0700

    allow rdj-bio javascript to be cached

---
+++ js/embed-soundcloud.js
@@ -0,0 +1,11 @@
+function embedSoundcloud(id) {
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+        if (r.readyState === 4 && r.status === 200) {
+            document.querySelector('#showcase').innerHTML = '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/' + JSON.parse(r.responseText).id + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
+        }
+    };
+
+    r.open('GET', 'http://api.soundcloud.com/resolve?url=http://soundcloud.com/' + id + '&client_id=27b0c9c2c33ba40bcb06b6026112a568', true);
+    r.send(null);
+}
